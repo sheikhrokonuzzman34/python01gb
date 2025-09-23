61ef408a3bfcfaf856eac20d104d87822a2c43da
sheikhrokonuzzman34 <mdrokon.u545@gmail.com>
2025-09-23 23:23:11 +0600

    add

---
+++ class_6.py
@@ -0,0 +1,67 @@
+# thislist = ["apple", "banana", "cherry","10","20","30",True,False,100,200]
+
+# print(len(thislist))
+
+
+# x = (1)   # x will be 1
+# y = (2.8) # y will be 2
+# z = ("3") # z will be 3
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# x = int(1)   # x will be 1
+# y = int(2.8) # y will be 2
+# z = int("3") # z will be 3
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# x = float(1)     # x will be 1.0
+# y = float(2.8)   # y will be 2.8
+# z = float("3")   # z will be 3.0
+# w = float("4.2") # w will be 4.2
+
+# thislist = ["apple", "banana", "cherry","watermelon","grapes","kiwi","mango"]
+
+# for x in thislist:
+#     if x == "cherry":
+#         continue
+#     elif x == "kiwi":
+#         continue
+#     elif x == "mango":
+#         break
+#     print(x)
+
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[2:4])
+# print(thislist[5])
+
+
+# thislist = ["Apple", "banana", "cherry"]
+# if "apple" in thislist:
+#   print("Yes, 'apple' is in the fruits list") 
+
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1] = "blackcurrant"
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# thislist[1:3] = ["blackcurrant", "watermelon"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1:3] = ["blackcurrant", "watermelon"]
+# print(thislist) 
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1:3] = ["watermelon"]
+# print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
